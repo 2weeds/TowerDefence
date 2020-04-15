d8effab9ddb1ebdbcfa6ec29790dad3ad5c792a3
Dovyds <Dovyds@users.noreply.github.com>
2020-04-16 01:08:30 +0300

    https://towerdefence.atlassian.net/browse/TOW-28

--- TowerDefence/Assets/Enemy.cs
+++ TowerDefence/Assets/Enemy.cs
@@ -9,24 +9,24 @@ public class Enemy : MonoBehaviour
     private int wavepointIndex = 0;
     void Start()
     {
-        target = Waypoints.points[0];
+        target = Waypoint.points[0];
     }
     void Update()
     {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed*Time.deltaTime, Space.World);
-        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
             GetNextWayPoint();
         }
     }
     void GetNextWayPoint()
     {
-        if (wavepointIndex >= Waypoints.points.Length - 1)
+        if (wavepointIndex >= Waypoint.points.Length - 1)
         {
             Destroy(gameObject);
         }
         wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
+        target = Waypoint.points[wavepointIndex];
     }
 }
