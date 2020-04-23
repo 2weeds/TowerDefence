023ee9cdf6fcb9c48e32c40c9e43158fa1afe7dd
Dovyds <Dovyds@users.noreply.github.com>
2020-04-23 12:24:08 +0300

    https://towerdefence.atlassian.net/browse/TOW-84 https://towerdefence.atlassian.net/browse/TOW-85

---
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+    void Start()
+    {
+        target = Waypoint.points[0];
+    }
+    void Update()
+    {
+        if (target!=null)
+        {
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+            if (Vector3.Distance(transform.position, target.position) <= 0.2)
+            {
+                
+                GetNextWayPoint();
+                transform.Rotate(dir);
+            }
+
+        }
+       
+    }
+    void GetNextWayPoint()
+    {
+        if (wavepointIndex >= Waypoint.points.Length - 1)
+        {
+            Destroy(gameObject);
+            wavepointIndex = -1;
+        }
+        else
+        {
+            wavepointIndex++;
+            target = Waypoint.points[wavepointIndex];
+        }
+        
+    }
+}
